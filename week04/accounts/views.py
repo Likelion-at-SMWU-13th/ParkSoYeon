47b6604a821ff676959a4117ac44ac45c3de3daa
ss0yaa <syp0205@sookmyung.ac.kr>
2025-04-01 20:59:32 +0900

    로그인 기능 추가

--- week04/accounts/views.py
+++ week04/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import SignupForm
+from django.contrib.auth import authenticate, login as auth_login
 
 def signup(request):
     if request.method == "POST":
@@ -12,4 +13,14 @@ def signup(request):
 
     return render(request, "signup.html", {"form" : form})
 
-# Create your views here.
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password) # 사용자 인증
+        if user is not None:
+            auth_login(request, user)
+            return redirect("main")
+    
+    return render(request, "login.html")
