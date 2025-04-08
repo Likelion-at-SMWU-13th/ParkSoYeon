53e1f9cd4075453ef9e74d14f600ef3507cd64bb
ss0yaa <syp0205@sookmyung.ac.kr>
2025-04-08 21:01:31 +0900

    [Feat] 나만의 운세 뽑기 프로그램 구현

---
+++ frontWeek01/week01.js
@@ -0,0 +1,30 @@
+/*화살표 함수
+function multiply1(x,y){
+    return x*y;
+}
+
+const result1 = multiply1(2,4);
+console.log(result1);
+
+const multiply2= (x,y) => {
+    return x*y;
+};
+*/
+// console.log(multiply2(2,4));
+
+/*Rest Parameter
+const multiplyAll = (...args) => {
+    return args.reduce((a,b) => a*b, 1);
+};
+
+console.log(multiplyAll(3,4,5));
+*/
+
+/*forEach
+const numbers = [1,2,3];
+const pows = [];
+
+numbers.forEach((item) => pows.push(item**2));
+console.log(pows);
+*/
+
