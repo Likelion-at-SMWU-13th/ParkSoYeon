bfbe249421283413d00dcffd333ef61ecfc87752
ss0yaa <syp0205@sookmyung.ac.kr>
2025-05-06 18:04:55 +0900

    fetch API 실습 완료

--- frontWeek03/js/movieList.js
+++ frontWeek03/js/movieList.js
@@ -13,3 +13,34 @@ const actionMovie = movies.filter((movie => {
 }))
 
 console.log("액이 포함된 영화 목록", actionMovie);
+
+async function fetchMovies(){
+    try{
+        const response = await fetch("movies.json"); // fetch 해올때까지 기다림
+        const movies = await response.json();
+        const container = document.getElementById("movieContainer");
+        const movieList = movies.filter((movie) =>  movie.genre === "액션");
+
+        movieList.forEach((movie) => {
+            const card = document.createElement("div");
+            card.className ="card";
+
+            card.innerHTML = `
+            <img src="${movie.poster}" alt = "${movie.title}">
+            <div class="card-body">
+                <h2 class="card-title">${movie.title}</h2>
+                <p class="card-rating">⭐ ${movie.rating}</p>
+                <p class="card-description">${movie.description}</p>
+            </div>
+            `;
+
+            container.appendChild(card);
+
+        });
+
+    } catch(error){
+        console.error("에러 발생: ", error)
+    }
+}
+
+fetchMovies();
