aa17443c1d51e826232e48a0ff2c096f1868aea6
ss0yaa <syp0205@sookmyung.ac.kr>
2025-05-20 20:41:28 +0900

    컴포넌트 실습 완료

---
+++ frontWeek04/week04/src/components/LionCard2.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import lion2 from "../assets/lion2.jpg";
+
+const LionCard2 = () => {
+  const isReal = true;
+
+  return (
+    <div>
+      <img src={lion2} />
+      <h1>2번 사자</h1>
+      <p>{isReal ? "진짜 아기사자" : "가짜 아기사자"}</p>
+    </div>
+  );
+};
+
+export default LionCard2;
