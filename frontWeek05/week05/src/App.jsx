ad07dd89431352421dbc78575e1dbff5651347c8
ss0yaa <syp0205@sookmyung.ac.kr>
2025-06-24 15:51:26 +0900

    [Feat] useState를 통한 상태 관리 구현

--- frontWeek05/week05/src/App.jsx
+++ frontWeek05/week05/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Hello from "./components/Hello";
+import Info from "./components/info";
 
 function App() {
   return (
     <>
-      <Hello name={"숙멋사"} />
+      {/* <Hello name={"숙멋사"} /> */}
+      <Info />
     </>
   );
 }
---
+++ frontWeek05/week05/src/components/Info.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const Info = () => {
+  const [name, setName] = useState("");
+  const [nickname, setNickname] = useState("");
+
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+
+  const onChangeNickName = (e) => {
+    setNickname(e.target.value);
+  };
+
+  return (
+    <div>
+      <br />
+      <div>
+        <input value={name} onChange={onChangeName} />
+        <input value={nickname} onChange={onChangeNickName} />
+      </div>
+      <div>
+        <div>
+          <b>이름: </b> {name}
+        </div>
+        <div>
+          <b>닉네임: </b> {nickname}
+        </div>
+      </div>
+      <br />
+      <br />
+    </div>
+  );
+};
+
+export default Info;
