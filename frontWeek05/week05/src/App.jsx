ab4777d1749b78dfce635afaf40c8eea74814cc8
ss0yaa <syp0205@sookmyung.ac.kr>
2025-06-24 17:04:52 +0900

    [Feat] 영화 목록 구현

--- frontWeek05/week05/src/App.jsx
+++ frontWeek05/week05/src/App.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./App.css";
 import Hello from "./components/Hello";
 import Info from "./components/info";
+import Movie from "./components/Movie";
 
 function App() {
   return (
     <>
       {/* <Hello name={"숙멋사"} /> */}
-      <Info />
+      <Movie />
     </>
   );
 }
