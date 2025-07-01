4545b2eb71a730c9c539497ea178d8f14b75b63e
ss0yaa <syp0205@sookmyung.ac.kr>
2025-07-01 16:58:36 +0900

    5,6. 중첩 라우팅, useNavigate 실습

--- frontWeek06/week06/src/App.jsx
+++ frontWeek06/week06/src/App.jsx
@@ -1,16 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 import QuizPage from "./pages/QuizPage";
 import ResultPage from "./pages/ResultPage";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="app">
-        <Routes>
-          <Route path="/" element={<QuizPage />} />
-          <Route path="/result" element={<ResultPage />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<QuizPage />} />
+          <Route path="quiz/:quizId" element={<QuizPage />} />
+          <Route path="result" element={<ResultPage />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 };
