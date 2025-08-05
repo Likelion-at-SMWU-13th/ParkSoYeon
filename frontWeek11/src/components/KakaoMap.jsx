596d03757ee0285e632a173cdf20fa32796c8da2
ss0yaa <syp0205@sookmyung.ac.kr>
2025-08-05 16:15:12 +0900

    [Feat] 현재 위치 가져오기 기능 구현

--- frontWeek11/src/components/KakaoMap.jsx
+++ frontWeek11/src/components/KakaoMap.jsx
@@ -1,9 +1,9 @@
-import { Map } from "react-kakao-maps-sdk";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
 
-const KakaoMap = () => {
+const KakaoMap = ({ center, isMarker }) => {
   return (
     <Map
-      center={{ lat: 33.450701, lng: 126.570667 }}
+      center={center}
       level={3}
       style={{
         width: "500px",
@@ -11,7 +11,9 @@ const KakaoMap = () => {
         borderRadius: "1rem",
         boxShadow: "0px 0px 10px 4px rgba(38,38,38,0.2)",
       }}
-    ></Map>
+    >
+      {isMarker && <MapMarker position={center}></MapMarker>}
+    </Map>
   );
 };
 
--- frontWeek11/src/components/MapContainer.jsx
+++ frontWeek11/src/components/MapContainer.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import KakaoMap from "./KakaoMap";
 import * as S from "./styled";
 
 const MapContainer = ({ title }) => {
+  const [center, setCenter] = useState({ lat: 33.450701, lng: 126.570667 });
+  const [isMarker, setIsMarker] = useState(false);
+
+  const handleFindMyLocation = () => {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const lat = pos.coords.latitude;
+      const lng = pos.coords.longitude;
+      setCenter({ lat, lng });
+      setIsMarker(true);
+    });
+  };
+
   return (
     <S.Wrapper>
       <S.H1>{title}</S.H1>
-      <KakaoMap />
+      <S.BtnContainer>
+        <S.FindBtn onClick={handleFindMyLocation}>현재 내 위치 찾기</S.FindBtn>
+      </S.BtnContainer>
+      <KakaoMap center={center} isMarker={isMarker} />
     </S.Wrapper>
   );
 };
