08bd4c66cd523ab7fd37ab905eb7c9ef1bae0315
ss0yaa <syp0205@sookmyung.ac.kr>
2025-09-02 19:38:29 +0900

    [Settings] 라우팅 파일 설정

--- frontWeek12/week12/src/App.jsx
+++ frontWeek12/week12/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "./App.css";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
