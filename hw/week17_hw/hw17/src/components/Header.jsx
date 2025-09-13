d89d424fd3b44304d8884a6cf2dc17dffd9b9c8b
ss0yaa <syp0205@sookmyung.ac.kr>
2025-09-14 00:52:56 +0900

    [Feat] 헤더 컴포넌트 생성

---
+++ hw/week17_hw/hw17/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import * as S from "../styles/Header.styles.js";
+
+function Header() {
+  return (
+    <S.Container>
+      <S.Title>배스킨라빈스 31</S.Title>
+      <S.Subtitle>원하는 맛을 아래에서 골라 맘껏 담아주세요 ~</S.Subtitle>
+    </S.Container>
+  );
+}
+
+export default Header;
---
+++ hw/week17_hw/hw17/src/styles/Header.styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  text-align: center;
+`;
+
+export const Title = styled.p`
+  font-size: 40px;
+`;
+
+export const Subtitle = styled.p`
+  font-size: 25px;
+`;
