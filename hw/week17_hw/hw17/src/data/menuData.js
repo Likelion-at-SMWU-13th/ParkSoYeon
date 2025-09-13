01bc62973df385a8c4542e83ebc054c20f918e04
ss0yaa <syp0205@sookmyung.ac.kr>
2025-09-14 00:38:15 +0900

    [Settings] 더미데이터 추가

---
+++ hw/week17_hw/hw17/src/data/menuData.js
@@ -0,0 +1,35 @@
+import menu1 from "../assets/menu1.png";
+import menu2 from "../assets/menu2.png";
+import menu3 from "../assets/menu3.png";
+import menu4 from "../assets/menu4.png";
+import menu5 from "../assets/menu5.png";
+import menu6 from "../assets/menu6.png";
+import menu7 from "../assets/menu7.png";
+import menu8 from "../assets/menu8.png";
+import menu9 from "../assets/menu9.png";
+import menu10 from "../assets/menu10.png";
+import menu11 from "../assets/menu11.png";
+import menu12 from "../assets/menu12.png";
+import menu13 from "../assets/menu13.png";
+import menu14 from "../assets/menu14.png";
+import menu15 from "../assets/menu15.png";
+import menu16 from "../assets/menu16.png";
+
+export const iceCreamMenus = [
+  { id: 1, name: "위대한 비쵸비", src: menu1 },
+  { id: 2, name: "블루 바나나 브륄레", src: menu2 },
+  { id: 3, name: "아몬드 봉봉", src: menu3 },
+  { id: 4, name: "엄마는 외계인", src: menu4 },
+  { id: 5, name: "트로피컬 썸머 플레이", src: menu5 },
+  { id: 6, name: "민트 초콜릿 칩", src: menu6 },
+  { id: 7, name: "뉴욕 치즈케이크", src: menu7 },
+  { id: 8, name: "레인보우 샤베트", src: menu8 },
+  { id: 9, name: "체리쥬빌레", src: menu9 },
+  { id: 10, name: "슈팅스타", src: menu10 },
+  { id: 11, name: "오레오 쿠키 앤 크림", src: menu11 },
+  { id: 12, name: "베리베리 스트로베리", src: menu12 },
+  { id: 13, name: "31요거트", src: menu13 },
+  { id: 14, name: "바람과 함께 사라지다", src: menu14 },
+  { id: 15, name: "피스타치오 아몬드", src: menu15 },
+  { id: 16, name: "그린티", src: menu16 },
+];
