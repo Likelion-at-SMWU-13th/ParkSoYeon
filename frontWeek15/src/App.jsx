22d09a8198643c0a6feef5e64918ed733fb95756
ss0yaa <syp0205@sookmyung.ac.kr>
2025-09-23 20:19:40 +0900

    [Feat] isError을 사용한 에러 처리 완료

--- frontWeek15/src/App.jsx
+++ frontWeek15/src/App.jsx
@@ -5,13 +5,19 @@ function App() {
   async function getPosts() {
     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
     return await response.json();
+    // throw new Error("error test");
   }
-  const { data: postsData, isPending } = useQuery({
+  const {
+    data: postsData,
+    isPending,
+    isError,
+  } = useQuery({
     queryKey: ["posts"],
     queryFn: getPosts,
   });
   // console.log("postsData", postsData);
   if (isPending) return "로딩 중입니다 ...";
+  if (isError) return "에러가 발생했습니다.";
   const posts = postsData ?? [];
 
   return (
