ab84bd503c8393dc992f5b75f6303a491f613872
ss0yaa <syp0205@sookmyung.ac.kr>
2025-09-23 20:04:31 +0900

    [Feat] useQuery를 사용한 데이터 조회 완료

--- frontWeek15/src/App.jsx
+++ frontWeek15/src/App.jsx
@@ -6,13 +6,22 @@ function App() {
     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
     return await response.json();
   }
-  const queryResult = useQuery({
+  const { data: postsData } = useQuery({
     queryKey: ["posts"],
     queryFn: getPosts,
   });
-  console.log("queryResult", queryResult);
+  console.log("postsData", postsData);
+  const posts = postsData ?? [];
 
-  return <>tanctack query 실습</>;
+  return (
+    <div>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
