efdb5cdbe5e9107553119c471cb2355a821c195b
ss0yaa <syp0205@sookmyung.ac.kr>
2025-09-23 19:51:56 +0900

    [Feat] QueryClientProvider 설정 완료

--- frontWeek15/src/App.jsx
+++ frontWeek15/src/App.jsx
@@ -1,17 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import "./App.css";
+
 function App() {
   async function getPosts() {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
     return await response.json();
   }
   const queryResult = useQuery({
     queryKey: ["posts"],
     queryFn: getPosts,
   });
-
   console.log("queryResult", queryResult);
-  return <>tanstack query 실습!</>;
+
+  return <>tanctack query 실습</>;
 }
 
 export default App;
