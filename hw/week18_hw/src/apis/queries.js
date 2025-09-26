e42cb670b2df01c056c50ac80bc40516d6b8de37
ss0yaa <syp0205@sookmyung.ac.kr>
2025-09-26 14:46:27 +0900

    [Feat] 마이페이지 조회 코드 작성

--- hw/week18_hw/src/apis/queries.js
+++ hw/week18_hw/src/apis/queries.js
@@ -1,5 +1,6 @@
-import { useMutation } from "@tanstack/react-query";
-import { createUser } from "../axios";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
+import { createUser, updateUser, readUser } from "../axios";
 
 // 회원가입 (POST)
 export const useCreateUser = () => {
@@ -13,8 +14,16 @@ export const useCreateUser = () => {
   });
 };
 
-// 개인정보 수정 (PUT)
-
 // 마이페이지 조회 (GET)
+export const useReadUser = (userId) => {
+  return useQuery({
+    queryKey: ["users", userId],
+    queryFn: () => readUser(userId),
+    staleTime: 30 * 1000,
+    retry: 3,
+  });
+};
+
+// 개인정보 수정 (PUT)
 
 // 회원 정보 수정 (DELETE)
